e1794d03014cd1245a5f79cc353145f5f3450846
yanzxu <yzxu@thoughtworks.com>
2022-04-16 20:17:23 +0800

    init datasource

---
+++ bff-service/src/main/java/com/xyz/bffservice/BffServiceApplication.java
@@ -0,0 +1,11 @@
+package com.xyz.bffservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BffServiceApplication.class, args);
+    }
+}
---
+++ store-service/src/main/java/com/xyz/storeservice/StoreServiceApplication.java
@@ -0,0 +1,11 @@
+package com.xyz.storeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StoreServiceApplication.class, args);
+    }
+}
