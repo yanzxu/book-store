467cf3bd0c2963ae6dc0209df56c4c867e58a426
yanzxu <906700211@qq.com>
2022-04-24 20:41:09 +0800

    k8s: add image_tag to shell

--- user-service/deployment/k8s/update-deploy-image.sh
+++ user-service/deployment/k8s/update-deploy-image.sh
@@ -1,8 +1,11 @@
+IMAGE_TAG=$1
+
 if test -z "$(kubectl get deploy deployment-user-service -n dev)"; then
-    echo "******* deployment-user-service not exist, create new deployment *******"
-    sed -e 's/{IMAGE_TAG}/46/g' deployment-user-service.template > deployment-user-service.yaml;
+    echo "******* deployment-user-service not exist, create new deployment with tag: $IMAGE_TAG *******"
+    sed -e "s/{IMAGE_TAG}/$IMAGE_TAG/g" deployment-user-service.template > deployment-user-service.yaml;
     kubectl apply -f ./deployment-user-service.yaml 
+    rm -rf ./deployment-user-service.yaml
 else
-    echo "******* deployment-user-service already exist, update image *******"
-    kubectl set image deployment/deployment-user-service user-service=yanzxu/user-service:45 -n dev
+    echo "******* deployment-user-service already exist, update image tag to: $IMAGE_TAG *******"
+    kubectl set image deployment/deployment-user-service user-service=yanzxu/user-service:$IMAGE_TAG -n dev
 fi
