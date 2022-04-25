245f330da4d9648994e99012b4573379d2c34562
yanzxu <yzxu@thoughtworks.com>
2022-04-25 15:28:43 +0800

    k8s: fix issue

--- user-service/deployment/k8s/update-deploy-image.sh
+++ user-service/deployment/k8s/update-deploy-image.sh
@@ -2,8 +2,9 @@ IMAGE_TAG=$1
 
 if test -z "$(kubectl get deploy deployment-user-service -n dev)"; then
     echo "******* deployment-user-service not exist, create new deployment with tag: $IMAGE_TAG *******"
-    sed -e "s/{IMAGE_TAG}/$IMAGE_TAG/g" deployment-user-service.template > deployment-user-service.yaml;
-    kubectl apply -f ./deployment-user-service.yaml 
+    sed -e "s/{IMAGE_TAG}/$IMAGE_TAG/g" deployment-user-service-template.yaml > deployment-user-service.yaml;
+    kubectl apply -f ./deployment-user-service.yaml
+    kubectl apply -f ./svc-user-service.yaml
     rm -rf ./deployment-user-service.yaml
 else
     echo "******* deployment-user-service already exist, update image tag to: $IMAGE_TAG *******"
