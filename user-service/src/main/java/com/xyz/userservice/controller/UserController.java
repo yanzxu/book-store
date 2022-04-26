8619cd1dbeaae5b8787330ce27666aaa72f6ccfd
yanzxu <yzxu@thoughtworks.com>
2022-04-26 19:35:06 +0800

    fix log4j dependency

--- user-service/src/main/java/com/xyz/userservice/controller/UserController.java
+++ user-service/src/main/java/com/xyz/userservice/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.xyz.userservice.controller;
 
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+@Log4j2
 @RestController
 @RequestMapping("/users")
 public class UserController {
